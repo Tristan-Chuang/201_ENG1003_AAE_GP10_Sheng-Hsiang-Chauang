fae53ece24d83af89ce9a940949f6b75f8f90dce
Tristan-Chuang <71874509+Tristan-Chuang@users.noreply.github.com>
2020-10-09 17:16:43 +0800

    Ahh

---
+++ Python first try.py
@@ -0,0 +1,19 @@
+print("hello, world")
+print("hello, my name is Tristan")
+
+a = 2
+b = 3
+c =a-b
+print("print c value")
+print(c)
+
+if b>a:
+    print("b is larger than a")
+
+#你好
+def multiplication(a,b):
+    c=a*b
+    print("print the value from multiplication")
+    print(c)
+
+    multiplication(1,10)
